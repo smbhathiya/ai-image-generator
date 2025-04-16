4689c84b39e449db9498591b81cf65daf6994b26
smbhathiya <smbhathiya@gmail.com>
2025-04-16 12:13:21 +0530

    feat: enhance dashboard layout with sidebar and header components; update color variables in globals.css; add react-icons dependency

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,27 @@
 import { ReactNode } from "react";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 function Dashboardlayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <div>
-        <div>{children}</div>
-      </div>
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="floating" />
+        <SidebarInset>
+          <div className="sticky top-0 z-30 bg-background">
+            <SiteHeader />
+          </div>
+          <div className="flex flex-1 flex-col">{children}</div>
+        </SidebarInset>
+      </SidebarProvider>
     </div>
   );
 }
