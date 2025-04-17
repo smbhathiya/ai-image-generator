67f48a524f2b44d3977569fcd614f689170f7a1d
smbhathiya <smbhathiya@gmail.com>
2025-04-17 10:15:29 +0530

    feat: enhance Dashboard layout with Card components and update AppSidebar to use SVG logo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,33 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Dashboard() {
   return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
+    <>
+      <div className="m-4">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-primary text-2xl font-bold">
+              Recent
+            </CardTitle>
+            <CardDescription>Comming soon</CardDescription>
+          </CardHeader>
+        </Card>
+      </div>
+      <div className="m-4">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-primary text-2xl font-bold">
+              History
+            </CardTitle>
+            <CardDescription>Comming soon</CardDescription>
+          </CardHeader>
+        </Card>
+      </div>
+    </>
   );
 }
