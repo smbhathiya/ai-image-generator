0a362e88df1ac9e7f1189bc7fe39a68fb30c1af5
smbhathiya <smbhathiya@gmail.com>
2025-04-17 11:02:38 +0530

    fix: remove user prop from NavUser component in AppSidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -70,7 +70,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavMain items={data.navMain} />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser />
       </SidebarFooter>
     </Sidebar>
   );
