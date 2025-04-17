6f3b250cf689da71b2cb1e5ea3d2eb19f5c22c68
smbhathiya <smbhathiya@gmail.com>
2025-04-17 11:00:20 +0530

    feat: add user greeting and loading state to Dashboard component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,39 @@
+"use client";
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useUser } from "@clerk/nextjs";
+
+function getGreeting() {
+  const hour = new Date().getHours();
+  if (hour < 12) return "Good morning";
+  if (hour < 18) return "Good afternoon";
+  return "Good evening";
+}
 
 export default function Dashboard() {
+  const { user } = useUser();
+  const isLoading = !user;
+  const userData = {
+    name: user?.fullName || user?.firstName || "User",
+  };
+  const greeting = getGreeting();
   return (
     <>
       <div className="m-4">
+        <h1 className="text-primary text-2xl sm:text-3xl lg:text-4xl font-bold p-4 mb-4">
+          {isLoading ? (
+            <Skeleton className="h-10 w-60" />
+          ) : (
+            <>
+              {greeting}, {userData.name} 👋
+            </>
+          )}
+        </h1>
         <Card>
           <CardHeader>
             <CardTitle className="text-primary text-2xl font-bold">
