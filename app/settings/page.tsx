0f098002f4348b3f35d1b9ce0035e06c65cc5200
smbhathiya <smbhathiya@gmail.com>
2025-10-04 14:40:30 +0530

    refactor: clean up SettingsPage component by removing unused imports and improving structure

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,15 +2,10 @@
 
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { useClerk } from "@clerk/nextjs";
 import { Separator } from "@radix-ui/react-dropdown-menu";
+import { Settings } from "lucide-react";
 
 export default function SettingsPage() {
   const { openUserProfile } = useClerk();
@@ -18,12 +13,10 @@ export default function SettingsPage() {
   return (
     <div className="m-4">
       <div className="max-w-4xl mx-auto">
-        <Card>
-          <CardHeader>
-            <CardTitle>Settings</CardTitle>
-            <CardDescription>
-              Manage your account and preferences
-            </CardDescription>
+        <Card className="bg-background border-0 shadow-none">
+          <CardHeader className="flex items-center gap-2">
+            <Settings className="w-6 h-6 gap-2" />
+            <CardTitle className="text-2xl font-bold">Settings</CardTitle>
           </CardHeader>
           <CardContent>
             <div className="flex items-center justify-between gap-4">
